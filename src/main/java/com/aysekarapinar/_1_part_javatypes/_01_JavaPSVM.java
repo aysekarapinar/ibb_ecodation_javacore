1bdcf89952e76e214dce97255e06c98d71160c78
Ayşe Karapinar <ayseozlemkarapinar28@outlook.com>
2025-02-06 21:06:06 +0300

    java core init

---
+++ src/main/java/com/aysekarapinar/_1_part_javatypes/_01_JavaPSVM.java
@@ -0,0 +1,11 @@
+package com.aysekarapinar._1_part_javatypes;
+
+
+public class _01_JavaPSVM {
+    // psvm + tab
+    public static void main(String[] args) {
+        // Çıktı
+        // sout + tab
+        System.out.println("Cennet");
+    } // end psvm
+} //end class
---
+++ src/main/java/com/aysekarapinar/_1_part_javatypes/_04_JavaWrapperTypes.java
@@ -0,0 +1,7 @@
+package com.aysekarapinar._1_part_javatypes;
+
+public class _04_JavaWrapperTypes {
+    public static void main(String[] args) {
+
+    }
+}
