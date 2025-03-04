8171c6a899bba40bd804a232e5419777f729d4cd
Ayşe Karapinar <ayseozlemkarapinar28@outlook.com>
2025-03-05 00:08:27 +0300

    Revized

---
+++ src/main/java/com/aysekarapinar/_1_week/_04_5_Cast.java
@@ -0,0 +1,7 @@
+package com.aysekarapinar._1_week;
+
+public class _04_5_Cast {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/aysekarapinar/_1_week/_08_Scanner.java
+++
@@ -1,4 +0,0 @@
-package com.aysekarapinar._1_week;
-
-public class _08_Scanner {
-}
