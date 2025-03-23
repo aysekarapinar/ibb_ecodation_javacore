a85a86769c47e4f522e71f7698e86b5c7cf55ce3
Ayşe Karapinar <ayseozlemkarapinar28@outlook.com>
2025-03-23 23:50:51 +0300

    system exit, break

--- src/main/java/com/aysekarapinar/_1_week/_08_2_System.java
+++ src/main/java/com/aysekarapinar/_1_week/_08_2_System.java
@@ -1,8 +1,34 @@
 package com.aysekarapinar._1_week;
 
-public class _08_2_System {
-    public static void main(String[] args) {
+import java.security.PublicKey;
 
-        //System
+public class _08_2_System {
+
+    //Eğer System.exit(0) kullanılırsa;
+    // 1- JVM durur
+    // 2- Çalışan tüm threadler sonlanır.
+    // 3- Programın işletim sistemindeki processlerde kapanır
+
+    public static void processCloseJVMStop(){
+        System.out.println("Program başladı, system exit(0)");
+        System.exit(0);
+        System.out.println("Bu satır çalışmayacak");
+    }
+
+    public static void processContinueJVMContinue() {
+        for (int i = 0; i < 10; i++) {
+            if (i==5) {
+                System.out.println("Program başladı, break");
+                System.out.println("Sadece döngüden çıkılıyor");
+                break;
+            }
+            System.out.println(i+". sıra");
+        }
+        System.out.println("Bu satır çalışacak");
+    }
+
+    public static void main(String[] args) {
+        //processCloseJVMStop();
+        processContinueJVMContinue();
     }
 }
